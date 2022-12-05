7e894915c2c97ea1d4babbaceb746605ac163b17
jjackie <jacky@rustningshield.com>
2022-12-05 20:34:04 -0300

    implementacion del DOM

--- js/shop.js
+++ js/shop.js
@@ -1,56 +1,28 @@
 /* Simulador de carrito de compras para comprar mercancías del juego o el juego*/
 
-let products = parseInt(
-    prompt("Type the number of the product you want to buy: 1.Ghosting (Full version) - 2.Ghosting Artbook - 3.Ghosting Bundle (Full game + Artbook)")
-);
+const shopProducts  = document.getElementById("shopProducts");
 
-let keepBuying = true;
-let buyTotal = 0;
-let wantTo;
 
-const productsArray = [];
-
-class Product{
-    constructor(id,name,price){
-    this.id = id
-    this.name = name;
-    this.price = price;
-    }
+const seeProducts = async () => {
+    const response = await fetch("../../products.json");
+    const products = await response.json();
+    return products;
 }
 
-const ghosting = new Product(1, "ghosting", 30);
-productsArray.push(ghosting);
-const artbook = new Product(2, "artbook", 27);
-productsArray.push(artbook);
-const bundlePack = new Product(3, "bundle", 51);
-productsArray.push(bundlePack);
+const data = seeProducts();
+console.log(data);
 
-console.log(productsArray);
-
-while(keepBuying === true) {
-    buyTotal = buyTotal + productsArray[products-1].price;
-
-    wantTo = parseInt(prompt("Do you want to keep buying? (Enter a number) 1.Yes - 2.No"));
-    if(wantTo === 1) {
-        products = parseInt(prompt("Type the number of the product you want to buy: 1.Ghosting (Full version) - 2.Ghosting Artbook - 3.Ghosting Bundle (Full game + Artbook)"));
-    } else if (wantTo === 2) {
-        keepBuying = false;
-    } else {
-        wantTo = alert("Invalid option.");
-        continue;
-    } 
-}
-alert("The total amount is " + buyTotal);
-console.log(buyTotal);
-
-function belowFifty (sales) {
-    return sales.price < 51;
-}
-
-console.log(productsArray.filter(belowFifty));
-
-function bundleFind (pack) {
-    return pack.name === "bundle";
-}
-
-console.log(productsArray.find(bundleFind));
+seeProducts().then(products => {
+    products.forEach(product => {
+        shopProducts.innerHTML += `
+        <div class = "card col-6 border-0">
+            <img src="../assets/shopimg/${product.img}" class ="card-img-top">
+            <div class="card-body">
+                <h2 class="card-title">${product.name}</p>
+                <p class="card-text">$${product.price}</p>
+                <button id= "shopButton" class="btn shoppingButton">Add to cart</button>
+            </div>
+        </div>
+        `
+    });
+});
